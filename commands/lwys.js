0a5c50848160fa2a5b19c55c11086d216327333f
jennasisis <jennasisis@gmail.com>
2017-11-08 14:05:41 -0500

    Update lwys.js

--- commands/lwys.js
+++ commands/lwys.js
@@ -1,8 +1,11 @@
 const Discord = require(‘discord.js’);
 
+var authorName = ""
+var authorAva = ""
+
 module.exports.run = (client, msg, args) => {
       msg.channel.send(new Discord.RichEmbed()
-            .setAuthor())
+            .setAuthor(authorName, authorAva))
 };
 
 module.exports.conf = {
