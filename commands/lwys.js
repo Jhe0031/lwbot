3a497786b5418640af93dba9f86148d337527ea9
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-08 20:37:45 -0500

    Push before I test things

--- commands/lwys.js
+++ commands/lwys.js
@@ -1,14 +1,23 @@
-const Discord = require(‘discord.js’);
-
-var authorName = ""
-var authorAva = ""
+const Discord = require('discord.js');
+const fs = require('fs');
+const authorData = require('../wtData/authors/shen.json');
+const webtoonData = require('../wtData/webtoons/lwys.json');
 
 module.exports.run = (client, msg, args) => {
+      
       msg.channel.send(new Discord.RichEmbed()
-            .setAuthor(authorName, authorAva)
-)
+            .setColor(54371)
+            .setAuthor(authorData.name, authorData.avatar, null)
+            .addField("Title:", webtoonData.name, true)
+            .addField("Author:", webtoonData.author, true)
+            .addField("Updates:", webtoonData.updates, true)
+            .addField("Summary:", `*${webtoonData.summary}*`, true)
+            .addField("URL:", `[Click Here](${webtoonData.URL})`, true)
+            .setThumbnail(webtoonData.avatar)
+      );
+
 };
 
 module.exports.conf = {
-      name: “lwys”
+      name: "lwys"
 }
