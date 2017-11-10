d059216fe5433498690aed99d3a3b9ce6854bba9
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-10 17:38:21 -0500

    screw this. making set its own command

--- commands/lwys.js
+++ commands/lwys.js
@@ -1,11 +1,8 @@
 const Discord = require('discord.js');
-const fs = require('fs');
 const authorData = require('../wtData/authors/shen.json');
 const webtoonData = require('../wtData/webtoons/lwys.json');
-const path = require('path');
 
 module.exports.run = (client, message, args) => {
-            if(args[0] === ""){
             message.channel.send(new Discord.RichEmbed()
                   .setColor(54371)
                   .setAuthor(authorData.name, authorData.avatar, null)
@@ -16,15 +13,4 @@ module.exports.run = (client, message, args) => {
                   .addField("URL:", `[Click Here](${webtoonData.URL})`, true)
                   .setThumbnail(webtoonData.avatar)
             );
-      }
-      if(args[0] === "set"){
-            const modRole = message.guild.roles.find("name", "Mods");
-            if (!message.member.roles.has(modRole.id)){
-                  message.reply("You can't use this command.");
-            } else {
-                  if(args[1] === "title"){
-                        console.log("I'm gonna change this. Just pushing it tho lmao");
-                  }
-            }
-      }
 };
