df1d4c100e662f74be2b48bf4c461f972af07d3f
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-10 16:12:35 -0500

    Oh boi. This is a big one

--- commands/lwys.js
+++ commands/lwys.js
@@ -2,31 +2,28 @@ const Discord = require('discord.js');
 const fs = require('fs');
 const authorData = require('../wtData/authors/shen.json');
 const webtoonData = require('../wtData/webtoons/lwys.json');
-var path = require('path');
+const path = require('path');
 
 module.exports.run = (client, message, args) => {
+            if(args[0] === ""){
             message.channel.send(new Discord.RichEmbed()
                   .setColor(54371)
                   .setAuthor(authorData.name, authorData.avatar, null)
-                  .addField("Title:", webtoonData.name, true)
+                  .addField("Title:", webtoonData.title, true)
                   .addField("Author:", webtoonData.author, true)
                   .addField("Updates:", webtoonData.updates, true)
                   .addField("Summary:", `*${webtoonData.summary}*`, true)
                   .addField("URL:", `[Click Here](${webtoonData.URL})`, true)
                   .setThumbnail(webtoonData.avatar)
             );
+      }
       if(args[0] === "set"){
             const modRole = message.guild.roles.find("name", "Mods");
             if (!message.member.roles.has(modRole.id)){
                   message.reply("You can't use this command.");
             } else {
                   if(args[1] === "title"){
-                        var webtoonPath = path.resolve("../wtData/webtoons/lwys.json").format();
-                        fs.writeFile(webtoonPath.name, args[2], function (err) {
-                              if (err) throw err;
-                              console.log('Replaced!');
-                              message.channel.send(":white_check_mark:");
-                            });
+                        console.log("I'm gonna change this. Just pushing it tho lmao");
                   }
             }
       }
