ec901f08935e1ad67d12c1c8a210943bf3408b64
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-22 22:09:46 -0500

    I AM SO CLOSE WITH THIS I CAN FUCKING TASTE IT

--- commands/myboo.js
+++ commands/myboo.js
@@ -1,6 +1,18 @@
 const Discord = require('discord.js');
 
-
 module.exports.run = (client, message, args) => {
-    
+    const parser = require('rss-parser');
+    var options = {
+        customFields: {
+          item: ['item'],
+        }
+      }
+      parser.parseURL('http://www.webtoons.com/en/drama/my-boo/rss?title_no=1185', options, function(err, parsed) {
+
+        parsed.feed.entries.forEach(function(entry) {
+          console.log(entry);
+        })
+      })
+
+      
 };
--- commands/rss.js
+++ commands/rss.js
@@ -2,15 +2,14 @@ module.exports.run = (client, message, args) => {
     const parser = require('rss-parser');
     var options = {
         customFields: {
-          feed: ['otherTitle', 'extendedDescription'],
-          item: ['coAuthor','subtitle'],
+          item: ['item'],
         }
       }
       parser.parseURL('http://www.webtoons.com/en/drama/my-boo/rss?title_no=1185', options, function(err, parsed) {
-        console.log(parsed.feed.extendedDescription);
+        console.log(parsed.feed.author);
        
         parsed.feed.entries.forEach(function(entry) {
-          console.log(entry.coAuthor + ':' + entry.subtitle);
+          console.log(entry);
         })
       })
 };
