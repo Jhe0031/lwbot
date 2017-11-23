cf5b4766d3067535f8877a09db499cab88226f47
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-22 21:46:21 -0500

    aaaaa im trying

--- commands/rss.js
+++ commands/rss.js
@@ -1,3 +1,16 @@
 module.exports.run = (client, message, args) => {
-      
+    const parser = require('rss-parser');
+    var options = {
+        customFields: {
+          feed: ['otherTitle', 'extendedDescription'],
+          item: ['coAuthor','subtitle'],
+        }
+      }
+      parser.parseURL('http://www.webtoons.com/en/drama/my-boo/rss?title_no=1185', options, function(err, parsed) {
+        console.log(parsed.feed.extendedDescription);
+       
+        parsed.feed.entries.forEach(function(entry) {
+          console.log(entry.coAuthor + ':' + entry.subtitle);
+        })
+      })
 };
