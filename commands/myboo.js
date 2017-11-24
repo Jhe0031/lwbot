330be6f5093882448768c3b8f4de4916c2f34bb2
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-11-23 21:19:06 -0500

    back to this *sigh*

--- commands/myboo.js
+++ commands/myboo.js
@@ -1,18 +1,20 @@
 const Discord = require('discord.js');
 
 module.exports.run = (client, message, args) => {
-    const parser = require('rss-parser');
-    var options = {
-        customFields: {
-          item: ['item'],
-        }
-      }
-      parser.parseURL('http://www.webtoons.com/en/drama/my-boo/rss?title_no=1185', options, function(err, parsed) {
-
-        parsed.feed.entries.forEach(function(entry) {
-          console.log(entry);
-        })
-      })
-
-      
+  const FeedMe = require('feedme');
+  const http = require('http');
+  
+  http.get('http://www.webtoons.com/en/drama/my-boo/rss?title_no=1185', (res) => {
+      var parser = new FeedMe(true);
+      res.pipe(parser);
+      parser.on('end', () => {
+          const data = parser.done();
+  
+          console.log(data.title);
+          console.log(data.link);
+          console.log(data.description);
+          console.log(data.image.url);
+          console.log(data.items[0].author);
+      });
+  });
 };
