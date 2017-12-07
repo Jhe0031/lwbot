55941d85123573ea43da769f8bd82ba092f86985
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-12-07 15:25:37 -0500

    Added some events

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,3 +1,3 @@
-module.exports.run = async (client) => {
-      message.guild.channels.find(“382585019300053015”).send(“Test”);
+module.exports.run = async (client, member, message, args) => {
+      await client.channels.get("235920926862999556").send(`<:white_plus:388418662564102146> Welcome, ${member.user} to the server! Don't forget to read <#383426031014379520> and <#382640041358262285>`);
 };
--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,4 @@
 module.exports.run = async (client) => {
     console.log(`Bot is online`)
+    client.channels.get("235920926862999556").send(":white_check_mark: **Bot is online!**");
 };
