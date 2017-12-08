e9b4d269df09126af47890bd3a6d0ace52b643d0
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-12-08 16:44:58 -0500

    renamed myboo -> template

--- commands/template.js
+++ commands/template.js
@@ -9,12 +9,12 @@ module.exports.run = (client, message, args) => {
       res.pipe(parser);
       parser.on('end', () => {
           const data = parser.done();
-  
+
           message.channel.send(new Discord.RichEmbed()
           .setColor(54371)
           .setAuthor(data.items[0].author, data.image.url, data.link)
           .addField("Title:", data.title, true)
-          .addField("Updates:", "EVERYDAY", true)
+          .addField("Updates:", "COMPLETED", true)
           .addField("Summary:", `*${data.description}*`, true)
           .addField("URL:", `[Click Here](${data.link})`, true)
           .setThumbnail(data.image.url)
