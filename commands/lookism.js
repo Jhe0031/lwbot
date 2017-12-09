35abd847c84bf96362e3682a7065af2ce17fcbdd
Samir Martin <samirbuch@Samirs-MacBook.local>
2017-12-09 15:54:15 -0500

    added lookism

---
+++ commands/lookism.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');
+
+module.exports.run = (client, message, args) => {
+  const FeedMe = require('feedme');
+  const http = require('http');
+ 
+  http.get('http://www.webtoons.com/en/drama/lookism/rss?title_no=1049', (res) => {
+      var parser = new FeedMe(true);
+      res.pipe(parser);
+      parser.on('end', () => {
+          const data = parser.done();
+
+          message.channel.send(new Discord.RichEmbed()
+          .setColor(54371)
+          .setAuthor(data.items[0].author, data.image.url, data.link)
+          .addField("Title:", data.title, true)
+          .addField("Updates:", "MON, SAT, SUN", true)
+          .addField("Description:", `*${data.description.trim()}*`, true)
+          .addField("URL:", `[Click Here](${data.link})`, true)
+          .setThumbnail(data.image.url)
+      );
+  });
+});
+};
