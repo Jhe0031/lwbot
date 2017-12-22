9adba9fa01489edf964fdd16e94f08d4d597b3e5
jennasisis <jennasisis@gmail.com>
2017-12-22 10:19:38 -0500

    added basic framework for legal command. will edit later because it's hard af to do on a phone

---
+++ commands/to-edit.js
@@ -0,0 +1,12 @@
+module.exports.run = (client, message, args) => {
+      const Discord = require ('discord.js');
+
+      if (message.guild.id === 
+382585019300053013){
+                               message.channel.send(new Discord.RichEmbed()
+      .setTitle("Legal Info")
+      .setDescription("Here you can find all needed legal information about this bot and the guild")
+      .setColor(<yellow>)
+);
+      }
+};
