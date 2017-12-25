c4f922da07e67e12cbfcbfa007655ce04feec5e3
jennasisis <jennasisis@gmail.com>
2017-12-25 13:15:22 -0500

    Delete guildMemberRemove.js

--- events/guildMemberRemove.js
+++
@@ -1,3 +0,0 @@
-module.exports.run = async (client, member, message, args) => {
-    await client.channels.get("235920926862999556").send(`<:white_minus:388418662341673014> Aww, **${member.user}** left the server! Guess they couldn't handle the hype..`);
-};
