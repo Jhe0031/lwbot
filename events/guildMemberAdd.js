4d0017fe44db44ab87c6fc0ebcf2660623987172
jennasisis <jennasisis@gmail.com>
2017-12-25 13:15:14 -0500

    Delete guildMemberAdd.js

--- events/guildMemberAdd.js
+++
@@ -1,3 +0,0 @@
-module.exports.run = async (client, member, message, args) => {
-      await client.channels.get("235920926862999556").send(`<:white_plus:388418662564102146> Welcome, ${member.user} to the server! Don't forget to read <#383426031014379520> and <#382640041358262285>`);
-};
