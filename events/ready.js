af7cd0751e925a48e679b110189e355d8abacd67
jennasisis <jennasisis@gmail.com>
2017-12-25 13:16:12 -0500

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,4 @@
 module.exports.run = async (client) => {
     console.log(`Bot is online`)
-    client.channels.get("235920926862999556").send(":white_check_mark: **Bot is online!**");
+    client.channels.get("382591354427146240").send(":white_check_mark: **Bot is online!**");
 };
