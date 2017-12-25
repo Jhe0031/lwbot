a8be35a228769cccb7c199456e063a7f542dd81a
jennasisis <jennasisis@gmail.com>
2017-12-25 13:18:10 -0500

    Delete info.js

--- commands/info.js
+++
@@ -1,8 +0,0 @@
-const Discord = require('discord.js');
-
-module.exports.run = (client, message, args) => {
-    message.channel.send(new Discord.RichEmbed()
-        .setThumbnail(client.avatarURL)
-        .setColor(54371)
-    )
-};
