6d4ea3abc1b7c90179b5e31a7828e9157721fcd6
jennasisis <jennasisis@gmail.com>
2017-12-25 13:17:16 -0500

    Delete green.js

--- commands/green.js
+++
@@ -1,8 +0,0 @@
-module.exports.run = (client, message) => {
-    const embed = {
-        "title": "Green",
-        "description": "54371",
-        "color": 54371
-    };
-    message.channel.send({ embed });
-};
