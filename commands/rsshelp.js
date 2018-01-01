8ba4b5e722e011aec56a621645a9ec2b730e5985
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-01 18:36:24 -0500

    finished rsshelp

---
+++ commands/rsshelp.js
@@ -0,0 +1,10 @@
+module.exports.run = async (client, message, args) => {
+    const Discord = require('discord.js');
+
+    await message.react("✅");
+    await message.author.send("**Hi there!** You've summoned me because you want to figure out how to grab the RSS link, right? I'll take you through the steps. Here we go!");
+    await message.author.send("** **");
+    await message.author.send("**Step 1:** Go to the Webtoon website (not the app), and pick the Webtoon you want the rss link of.", {files: ['./rsshelp-media/step1.gif']});
+    await message.author.send("** **");
+    await message.author.send("**Step 2:** Click on the button next to the subscribe button (it's two curved lines and a dot). Copy the URL you see at the top. This is your RSS link to paste into lw-wa.", {files: ['./rsshelp-media/step2.gif']});
+};
