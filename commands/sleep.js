cb0d0c2b581271c7bf2d0b7111ac294f5f8fb5db
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-01 18:36:15 -0500

    updated sleep + nosleeps

--- commands/sleep.js
+++ commands/sleep.js
@@ -1,7 +1,7 @@
 module.exports.run = (client, message, args) => {
     const Discord = require('discord.js');
     try {
-        var sleeps = ['./nosleeps/go-to-sleep-already.png', './nosleeps/nosleep.jpg', './nosleeps/its-time-to-sleep.png', './nosleeps/cya.png', './nosleeps/sleep-early.png', './nosleeps/less-bait.png', './nosleeps/lets-sleep.png', './nosleeps/kotatsu.png', './nosleeps/stay-up.png', './nosleeps/friend.png', './nosleeps/bed.png', './nosleeps/alright.jpg', './nosleeps/megaphone.png', './nosleeps/ok.jpg', './nosleeps/sheep.jpg', './nosleeps/drill.png', './nosleeps/fish.png'];
+        var sleeps = ['./nosleeps/go-to-sleep-already.png', './nosleeps/nosleep.jpg', './nosleeps/its-time-to-sleep.png', './nosleeps/cya.png', './nosleeps/sleep-early.png', './nosleeps/less-bait.png', './nosleeps/lets-sleep.png', './nosleeps/kotatsu.png', './nosleeps/stay-up.png', './nosleeps/friend.png', './nosleeps/bed.png', './nosleeps/alright.jpg', './nosleeps/megaphone.png', './nosleeps/ok.jpg', './nosleeps/sheep.jpg', './nosleeps/drill.png', './nosleeps/fish.png', './nosleeps/sleepsheep.jpg', './nosleeps/gotobed.jpg'];
     
         Array.prototype.randomElement = function (array) {
             return array[Math.floor(Math.random() * array.length)]
