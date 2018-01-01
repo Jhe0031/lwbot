5a6119a2dc63a0028c94fb578cfdcd8578b52bee
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-01 18:37:56 -0500

    added stats

---
+++ commands/stats.js
@@ -0,0 +1,15 @@
+module.exports.run = (client, message, args) => {
+    const Discord = require('discord.js');
+
+    var hours = (Math.round(client.uptime / (1000 * 60 * 60)))
+    var days = (Math.floor(hours / 24)) 
+    var finHours = (hours - days * 24) 
+    var minutes = (Math.round(client.uptime / (1000 * 60)) % 60)
+    var seconds = (Math.round(client.uptime / 1000) % 60)
+
+    message.channel.send(new Discord.RichEmbed()
+        .setAuthor(client.user.username, client.user.avatarURL)
+        .addField("Uptime:", `${days} days, ${finHours} hours, ${minutes} minutes, ${seconds} seconds`)
+        .addField("Ping:", `${Math.round(client.ping)}ms`)
+    );
+};
