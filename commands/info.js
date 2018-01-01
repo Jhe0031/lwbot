be339f337443cc49b3bf01554f523f4f473b926a
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-01 12:44:25 -0500

    added sleep, info

---
+++ commands/info.js
@@ -0,0 +1,3 @@
+module.exports.run = (client, message, args) => {
+    message.channel.send("Akii forgot to set this command up. Baka :anger:");
+}
---
+++ commands/sleep.js
@@ -0,0 +1,16 @@
+module.exports.run = (client, message, args) => {
+    const Discord = require('discord.js');
+    try {
+        var sleeps = ['./nosleeps/go-to-sleep-already.png', './nosleeps/nosleep.jpg', './nosleeps/its-time-to-sleep.png', './nosleeps/cya.png', './nosleeps/sleep-early.png', './nosleeps/less-bait.png', './nosleeps/lets-sleep.png', './nosleeps/kotatsu.png', './nosleeps/stay-up.png', './nosleeps/friend.png', './nosleeps/bed.png', './nosleeps/alright.jpg', './nosleeps/megaphone.png', './nosleeps/ok.jpg', './nosleeps/sheep.jpg', './nosleeps/drill.png', './nosleeps/fish.png'];
+    
+        Array.prototype.randomElement = function (array) {
+            return array[Math.floor(Math.random() * array.length)]
+        }
+    
+        var randomSleep = sleeps.randomElement(sleeps)
+
+        message.channel.send("", {files: [randomSleep]});
+    } catch (err) {
+        message.channel.send(`Whoops! Something went wrong! ${err}`);
+    }
+};
