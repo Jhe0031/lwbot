1adf899b3ba3174cd4630ca4a3e1a1ebcb2dc04f
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-04 13:17:31 -0500

    edited

--- events/ready.js
+++ events/ready.js
@@ -1,4 +1,28 @@
 module.exports.run = async (client) => {
-    console.log(`Bot is online`)
+    console.log(`Bot is online`);
     client.channels.get("382591354427146240").send(":white_check_mark: **Bot is online!**");
+
+    const Sequelize = require('sequelize');
+    const sequelize = new Sequelize('database', 'user', 'password', {
+        host: 'localhost',
+        dialect: 'sqlite',
+        logging: false,
+        // SQLite only
+        storage: 'database.sqlite',
+    });
+    
+    const Tags = sequelize.define('tags', {
+        name: {
+            type: Sequelize.STRING,
+            unique: true,
+        },
+        description: Sequelize.TEXT,
+        username: Sequelize.STRING,
+        usage_count: {
+            type: Sequelize.INTEGER,
+            defaultValue: 0,
+            allowNull: false,
+        },
+      });
+    Tags.sync();
 };
