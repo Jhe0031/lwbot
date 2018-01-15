e3747e9052af39eb8ffc724d4e2aea6138d7bf25
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-15 15:33:29 -0500

    formatting~

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -1,12 +1,16 @@
 module.exports.run = async (client, message, args, currency) => {
+    const Discord = require('discord.js')
+    
     const { Users, CurrencyShop } = require('../dbObjects');
     const user = await Users.findByPrimary(message.author.id);
-    return message.channel.send(
-        currency.sort((a, b) => b.balance - a.balance)
+    var leaderboardData = currency.sort((a, b) => b.balance - a.balance)
             .filter(user => client.users.has(user.user_id))
             .first(15)
-            .map((user, position) => `(${position + 1}) ${(client.users.get(user.user_id).tag)}: ${user.balance} Kowoks 💰`)
-            .join('\n'),
-        { code: true }
+            .map((user, position) => `\`-${position + 1}-\` ${(client.users.get(user.user_id).username)}: **${user.balance} Kowoks** 💰`)
+            .join('\n')
+
+    message.channel.send(new Discord.RichEmbed()
+        .addField("Leaderboard", leaderboardData)
+        .setColor(54371)
     );
 }
