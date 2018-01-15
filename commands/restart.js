a5d23ebb43445583ff35b5b9847dcf23c4ae7ee1
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-15 11:48:32 -0500

    akii & nen

--- commands/restart.js
+++ commands/restart.js
@@ -1,10 +1,11 @@
 module.exports.run = async (client, message, args) => {
     const Discord = require('discord.js');
+    var ids = [require('../config.json').ids.akii, require('../config.json').ids.nen]
 
-    if(message.author.id !== require('../config.json').ids.akii) return message.channel.send(":x: This command is limited to Akii only!");
+    if(message.author.id !== ids.some) return message.channel.send(":x: This command is limited to Akii & Nen only!");
 
     message.channel.send(":gear: **Bot is restarting...**");
     require('child_process').exec('pm2 restart main', (e, out, err) => {
-        client.users.get("107599228900999168").send(`:white_check_mark: **Bot has restarted**`);
+        console.log('Bot has finished restarting');
     });
 };
