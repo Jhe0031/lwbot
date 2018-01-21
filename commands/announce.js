5745bc142e4b1a6090fd3bb3fc86d6938157fc6d
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-20 19:55:55 -0500

    edited

--- commands/announce.js
+++ commands/announce.js
@@ -2,7 +2,7 @@ module.exports.run = async (client, message, args) => {
     try {
     const Discord = require('discord.js');
 
-    if(message.member.roles.has("382591919772925962")){
+    if(message.member.roles.has("382591919772925962") || message.author.id === "107599228900999168"){
         var date = new Date();
 
         String.prototype.replaceAll = function(search, replacement) {
@@ -20,19 +20,26 @@ module.exports.run = async (client, message, args) => {
         if(!part2) return message.channel.send(`:x: Missing the content!`);
         var content = part2.replaceAll('/n', '\n').trim();
         
-        if(splitter[2]) if(splitter[2].includes('no-subs')){announce(); message.channel.send(`:white_check_mark: **Announcement sent!**\n**Args:** \`${cmdargs}\``) } else return;
-        else {message.guild.channels.get('382642103626498049').send(`<@&383439861463515136>`); announce(); message.channel.send(`:white_check_mark: **Announcement sent!**`)}
-
+        var color = 54371
+        var thumbnail = client.user.avatarURL
+        if(cmdargs){
+            if(cmdargs.includes('color=')) {color = cmdargs.substring(cmdargs.indexOf('color=')+6);} else {color = 54371;}
+            if(cmdargs.includes('no-subs')) {announce();} else {message.guild.channels.get('382642103626498049').send(`<@&383439861463515136>`); announce();}
+        } else {color = 54371; message.guild.channels.get('382642103626498049').send(`<@&383439861463515136>`); announce();}
+        
         function announce(){
             message.guild.channels.get('382642103626498049').send(new Discord.RichEmbed()
-                .setColor(54371)
+                .setColor(color)
                 .setAuthor(message.author.username, message.author.avatarURL)
                 .setFooter(`${date.getHours()}:${date.getMinutes()} • ${date.getMonth()+1}/${date.getDate()}/${date.getFullYear()}`)
                 .addField(title, content)
             );
+            if(cmdargs) message.channel.send(`:white_check_mark: **Announcement sent!** | **Args:** \`${cmdargs}\``);
+            else message.channel.send(`:white_check_mark: **Announcement sent!**`);
         }
     } else message.channel.send(":x: You do not have access to this command!");
 } catch (err) {message.channel.send(`:x: ${err}`)}
 }
 
 //<@&383439861463515136> anno subs role
+//382642103626498049 announcement channel id
