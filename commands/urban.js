6b4c90cf75fbb4fb9211f43e3186e639d421056f
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-01-21 14:53:52 -0500

    added urban dictionary command

---
+++ commands/urban.js
@@ -0,0 +1,22 @@
+module.exports.run = (client, message, args) => {
+    const ud = require('urban-dictionary');
+    const Discord = require(`discord.js`);
+
+    var definition = args[0];
+
+    if(!definition) return message.channel.send(`:x: You forgot a word to look up!`);
+
+    ud.term(definition, function (error, entries, tags, sounds) {
+        if (error) {
+            if(error) message.channel.send(`:x: I couldn't find ${definition}`);
+        } else {
+            message.channel.send(new Discord.RichEmbed()
+                .addField(entries[0].word, entries[0].definition, true)
+                .addField("Example:", `*${entries[0].example}*`)
+                .setColor(54371)
+            );
+        }
+    });
+}
+
+
