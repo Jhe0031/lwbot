f241375141c6b6f1f14910754b7b545e509fec48
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-02-15 19:32:33 -0500

    Added hastebin-gen package to work with eval

--- commands/eval.js
+++ commands/eval.js
@@ -1,28 +1,35 @@
-module.exports.run = (client, message, args) => {
-    const Discord = require('discord.js');
-    const config = require('./config.json');
+const { inspect } = require("util");
+const { post } = require("snekfetch");
 
-    function clean(text) {
-        if (typeof(text) === "string")
-          return text.replace(/`/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
-        else
-            return text;
-      }
-
-    if(message.author.id !== "107599228900999168"){
-      message.channel.send(":x:");
+exports.run = async (client, message, args, level) => { // eslint-disable-line no-unused-vars
+  const code = args.join(" ");
+  const token = client.token.split("").join("[^]{0,2}");
+  const rev = client.token.split("").reverse().join("[^]{0,2}");
+  const filter = new RegExp(`${token}|${rev}`, "g");
+  if(message.author.id !== "107599228900999168") return;
+  try {
+    let output = eval(code);
+    if (output instanceof Promise || (Boolean(output) && typeof output.then === "function" && typeof output.catch === "function")) output = await output;
+    output = inspect(output, { depth: 0, maxArrayLength: null });
+    output = output.replace(filter, "[TOKEN]");
+    output = clean(output);
+    if (output.length < 1950) {
+      message.channel.send(`\`\`\`js\n${output}\n\`\`\``);
     } else {
-        try {
-          const code = args.join(" ");
-          let evaled = eval(code);
-    
-          if (typeof evaled !== "string")
-            evaled = require("util").inspect(evaled);
-    
-          message.channel.send(clean(evaled), {code:"xl"});
-        } catch (err) {
-          message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
-        }
+      try {
+        const { body } = await post("https://www.hastebin.com/documents").send(output);
+        message.channel.send(`Output was to long so it was uploaded to hastebin https://www.hastebin.com/${body.key}.js `);
+      } catch (error) {
+        message.channel.send(`I tried to upload the output to hastebin but encountered this error ${error.name}:${error.message}`);
+      }
     }
-    };
-    
+  } catch (error) {
+    message.channel.send(`The following error occured \`\`\`js\n${error.stack}\`\`\``);
+  }
+
+  function clean(text)  {
+    return text
+      .replace(/`/g, "`" + String.fromCharCode(8203))
+      .replace(/@/g, "@" + String.fromCharCode(8203));
+  }
+};
