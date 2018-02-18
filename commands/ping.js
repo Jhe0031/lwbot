81af6121af5a9089b0e53c9844a986bfc84399d0
Samir Martin <samirbuch@Samirs-MacBook.local>
2018-02-18 12:24:21 -0500

    eslint

--- commands/ping.js
+++ commands/ping.js
@@ -1,17 +1,17 @@
-const Discord = require('discord.js');
+const Discord = require(`discord.js`);
 
 module.exports.run = (client, message) => {
     message.channel.send(new Discord.RichEmbed()
-        .addField(":ping_pong: Pong!", "Pinging...")
-        .addField("left_right_arrow: Latency:", "Pinging...")
+        .addField(`:ping_pong: Pong!`, `Pinging...`)
+        .addField(`left_right_arrow: Latency:`, `Pinging...`)
         .setColor(54371)
     ).then(thismessage => {
         thismessage.delete();
         message.channel.send(new Discord.RichEmbed()
-        .addField(":ping_pong: Pong!", Math.round(client.ping)+"ms")
-        .addField(":left_right_arrow: Latency:", `${thismessage.createdAt-message.createdAt}ms`)
-        .setColor(54371)
-);
+            .addField(`:ping_pong: Pong!`, Math.round(client.ping)+`ms`)
+            .addField(`:left_right_arrow: Latency:`, `${thismessage.createdAt-message.createdAt}ms`)
+            .setColor(54371)
+        );
     });
 };
 
@@ -19,12 +19,12 @@ exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
-    permLevel: "User"
-  };
+    permLevel: `User`
+};
   
-  exports.help = {
-    name: "ping",
-    category: "Miscelaneous",
-    description: "It... like... pings. Then Pongs. And it\"s not Ping Pong.",
-    usage: "ping"
-  };
+exports.help = {
+    name: `ping`,
+    category: `Miscelaneous`,
+    description: `It... like... pings. Then Pongs. And it"s not Ping Pong.`,
+    usage: `ping`
+};
