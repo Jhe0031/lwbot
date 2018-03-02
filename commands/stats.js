f6b8219d1620aed20d7712614eba47c793b9d132
jennasisis <jennasisis@gmail.com>
2018-03-02 16:35:42 -0500

    updated stats command

--- commands/stats.js
+++ commands/stats.js
@@ -1,16 +1,26 @@
-module.exports.run = (client, message) => {
-    const Discord = require(`discord.js`);
+const { version } = require(`discord.js`);
+const Discord = require(`discord.js`);
+const moment = require(`moment`);
+require(`moment-duration-format`);
 
-    var hours = (Math.round(client.uptime / (1000 * 60 * 60)));
-    var days = (Math.floor(hours / 24)); 
-    var finHours = (hours - days * 24); 
-    var minutes = (Math.round(client.uptime / (1000 * 60)) % 60);
-    var seconds = (Math.round(client.uptime / 1000) % 60);
-
+exports.run = (client, message, args) => {
+    const duration = moment.duration(client.uptime).format(` D [days], H [hrs], m [mins], s [secs]`);
     message.channel.send(new Discord.RichEmbed()
-        .setAuthor(client.user.username, client.user.avatarURL)
-        .addField(`Uptime:`, `${days} days, ${finHours} hours, ${minutes} minutes, ${seconds} seconds`)
-        .addField(`Ping:`, `${Math.round(client.ping)}ms`)
+        .setTitle(`\`Statistics\``)
+        .addField(`Mem Usage`, `${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB`)
+        .addField(`Uptime`, duration)
+        .addField(`Ping`, `${Math.round(client.ping)}ms (Last three: ${client.pings})`)
+        .addField(`Discord.js`, `v${version}`)
+        .addField(`Node`, process.version)
         .setColor(`0x59D851`)
     );
+    
+    /* message.channel.send(`= STATISTICS =
+        • Mem Usage  :: ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB
+        • Uptime     :: ${duration}
+        • Users      :: ${client.users.size.toLocaleString()}
+        • Servers    :: ${client.guilds.size.toLocaleString()}
+        • Channels   :: ${client.channels.size.toLocaleString()}
+        • Discord.js :: v${version}
+        • Node       :: ${process.version}`, {code: `asciidoc`}); */
 };
