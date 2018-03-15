caddefc85c391d1d352ae8e675234a616bb0681b
jennasisis <jennasisis@gmail.com>
2018-03-15 17:30:13 -0400

    emit never worked properly anyway

--- commands/emit.js
+++
@@ -1,5 +0,0 @@
-module.exports.run = (client, message, args) => {
-  console.log(`Emit command ran!`);
-  if (message.author.id !== `107599228900999168`) return;
-  else {client.emit(args[0], args[1]);}
-};
